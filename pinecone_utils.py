66b39bd0f1a4a36c4cefa7955680f10cf9f8e88a
hakansenipek <hakansenipek@gmail.com>
2025-05-03 10:27:23 +0300

    Update pinecone_utils.py

--- pinecone_utils.py
+++ pinecone_utils.py
@@ -28,7 +28,7 @@ def init_pinecone():
                 name=index_name,
                 dimension=3072,  # text-embedding-3-large için
                 metric="cosine",
-                spec=ServerlessSpec(cloud="gcp", region="europe-west4-gcp") 
+                spec=ServerlessSpec(cloud="gcp", region="europe-west4") 
             )
             st.success(f"Indeks oluşturuldu: {index_name}")
 
